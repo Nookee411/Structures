c9820a270c1fbddb1a3a9dbead7e1f8af5bd9e94
Nookee411 <daniil43rus@list.ru>
2020-03-11 13:04:37 +0300

    Double ordered list added

---
+++ OrderedList/OrderedList.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Classes
+{
+    public class Node<T>
+    {
+        public T val { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Prev { get; set; }
+        public Node(T val)
+        {
+            this.val = val;
+        }
+    }
+    public class DoubleOrderedList<T> : IEnumerable<T>
+    {
+        Node<T> head;
+        Node<T> tail;
+        int count;
+
+        //Constructors
+        public DoubleOrderedList() //Empty consrtuctor
+        {
+            
+            this.head = null;
+            tail = null;
+            count = 0;
+        }
+        public DoubleOrderedList(T []Data)
+        {
+            foreach(T el in Data)
+            {
+                this.Add(el);
+            }
+        }
+        public int Count => count;
+        public bool IsEmpty => (count == 0);
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+            List<int> a = new List<int>();
+        }
+        
+        public void AddRange(IEnumerable<T>collection)
+        {
+            foreach (T el in collection)
+                this.Add(el);
+        }
+
+        public void Erase(int index)
+        {
+            if (index >= count)
+                throw new Exception("Index out of range");            
+            if(index < count/2)
+            {
+                if (count == 1)
+                    this.Clear();
+                else if (index == 0)
+                    this.PopFront();
+                else if (index == count - 1)
+                    this.PopBack();
+                else
+                {
+                    Node<T> todelete = head;
+                    for (int i = 0; i < index; i++)
+                    {
+                        todelete = todelete.Next;
+                    }
+                    todelete.Prev.Next = todelete.Next;
+                    todelete.Next.Prev = todelete.Prev;
+                }
+            }
+            else
+            {
+                Node<T> todelete = tail;
+                for(int i=0;i!=(count-1)-index;i++)
+                {
+                    todelete = todelete.Prev;
+                    i++;
+                }
+                if(index!=0)
+                    todelete.Prev.Next = todelete.Next;
+                if(index!= count-1)
+                    todelete.Next.Prev = todelete.Prev;
+            }
+            count--;
+
+        }
+
+        public void PopFront()
+        {
+            head = head.Next;
+            head.Prev = null;
+        }
+        public void PopBack()
+        {
+            tail = tail.Prev;
+            tail.Next = null;
+        }
+        
+        public void Add(T Data)
+        {
+            Node<T> temp = new Node<T>(Data);
+            if (head == null)
+                head = temp;
+            else
+            {
+                tail.Next = temp;
+                temp.Prev = tail;
+            }
+            tail = temp;
+            count++;
+        }
+        public void AddFront(T Data)
+        {
+            Node<T> temp = new Node<T>(Data);
+            if (head == null)
+                head = temp;
+            else
+            {
+                temp.Next = head;
+                head.Prev = temp;
+                head = temp;
+            }
+            count++;
+        }
+        //Override
+
+        //Output
+        public override string ToString()
+        {
+            Node<T> temp = head;
+            string res = "";
+            for (int i = 0; i < count&&temp!=null; i++, temp = temp.Next)
+                res += temp.val.ToString()+" ";
+            return res;
+        }
+
+        //equals
+        public override bool Equals(object obj)
+        {
+            DoubleOrderedList<T> b = (DoubleOrderedList<T>)obj;
+            if (this.count != b.count)
+                return false;
+            Node<T> temp1 = this.head;
+            Node<T> temp2 = b.head;
+            for (int i = 0;i<this.count;i++)
+            {
+                if(!temp1.val.Equals(temp2.val))
+                {
+                    return false;
+                }
+                temp1 = temp1.Next;
+                temp2 = temp2.Next;
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return int.Parse(this.head.val.ToString());
+        }
+
+        //Indexing
+        public T this[int index]
+        {
+            get 
+            {
+                if (index < 0 && index >= this.count)
+                    throw new Exception("Index out of range.");
+                //Prev and next allows us to get and set index at n/2
+                int i = 0;
+                if (index < count / 2)
+                {
+                    Node<T> temp = head;
+                    while (index !=i)
+                    {
+                        temp = temp.Next;
+                        i++;
+                    }
+                    return temp.val;
+                }
+                else
+                {
+                    Node<T> temp = tail;
+                    while ((count-1)-index != i)
+                    {
+                        temp = temp.Prev;
+                        i++;
+                    }
+                    return temp.val;
+                }
+            }
+
+            set
+            {
+                if (index < 0 && index >= this.count)
+                    throw new Exception("Index out range.");
+                int i = 0;
+                if (index < count / 2)
+                {
+                    Node<T> temp = head;
+                    while (i != index)
+                    {
+                        temp = temp.Next;
+                        i++;
+                    }
+                    temp.val = value;
+                }
+                else
+                {
+                    Node<T> temp = tail;
+                    while ((count - 1) - index != i)
+                    {
+                        temp = temp.Prev;
+                        i++;
+                    }
+                    temp.val = value;
+                }
+                return;
+            }
+        }
+
+        //IEnumerable realization
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.val;
+                current = current.Next;
+            }
+        }
+
+        public IEnumerable<T> BackEnumerator()
+        {
+            Node<T> current = tail;
+            while (current != null)
+            {
+                yield return current.val;
+                current = current.Prev;
+            }
+        }
+    }
+}
+
+
+//как перегрузить индексатор чтобы возвращались разные значения
+//как сделать перегрузки GetHashCode()
---
+++ OrderedList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classes;
+
+namespace OrderedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleOrderedList<int> a = new DoubleOrderedList<int>();
+            a.Add(5);
+            a.AddFront(4);
+            a.AddFront(3);
+            a.Add(6);
+            Console.WriteLine(a);
+            Console.WriteLine(a[0]);
+            a[3] = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(a[1]);
+            DoubleOrderedList<int> b = new DoubleOrderedList<int>(){ 3, 4, 5, 10 };
+            Console.WriteLine($"b = {b}");
+            if (a.Equals(b))
+                Console.WriteLine("Equal");
+            else
+                Console.WriteLine("Not equal");
+            b.Erase(3);
+            Console.WriteLine(b);
+            b.Erase(0);
+            Console.WriteLine(b);
+        }
+    }
+}
